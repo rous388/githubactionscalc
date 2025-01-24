6948176042a09bfa03c22fef5eb8f48686e69375
Victor <zerasul@gmail.com>
2025-01-24 09:47:36 +0100

    updating tests

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,35 +1,37 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void plus() {
         int a=1;
         int b=2;
         assertEquals(3,Calc.plus(a,b));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void substract() {
         int a=1;
         int b=2;
         assertEquals(1,Calc.substract(b,a));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void multiply() {
         int a=1;
         int b=2;
         assertEquals(2,Calc.multiply(a,b));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void divByZero() {
        RuntimeException exception= assertThrows(RuntimeException.class,()->Calc.div(2,0));
        assertTrue(exception.getMessage().contains("Zero"));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void div() {
         assertEquals(2,Calc.div(2,1));
     }
